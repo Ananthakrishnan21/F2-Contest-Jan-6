be8587de5d9aee150c4c38ed2dbd50f47c15a5be
Ananthakrishnan21 <ananthakrishnan430@gmail.com>
2023-01-07 11:25:35 +0530

    initial commit

--- script.js
+++ script.js
@@ -5,23 +5,57 @@ let arr = [
   { id: 2, name: "jack", age: "20", profession: "developer" },
   { id: 3, name: "karen", age: "19", profession: "admin" },
 ];
+let ans = [
+  { id: 11, name: "ramesh", age: "40", profession: "programmer" },
+  { id: 21, name: "sumesh", age: "41", profession: "financer" },
+  { id: 31, name: "mukhesh", age: "42", profession: "freelancer" },
+]
 
 function PrintDeveloperswithMap() {
   //Write your code here
+  let dev="developer"
+  for(let i=0;i<arr.length;i++){
+    if(arr[i].profession==dev){
+      console.log(arr[i].name)
+    }
+  }
 }
 
 function PrintDeveloperbyForEach() {
   //Write your code here
+  let dev="developer"
+  for(let i=0;i<arr.length;i++){
+    if(arr[i].profession==dev){
+      console.log(arr[i].name)
+    }
+  }
 }
 
 function addData() {
   //Write your code here
+  let obj={
+    id:4,
+    name:"susan",
+    age:"20",
+    profession:"intern"
+  }
+  arr.push(obj)
+  console.log(arr)
 }
 
 function removeAdmin() {
   //Write your code here
+  let dev="admin"
+  for(let i=0;i<arr.length;i++){
+    if(arr[i].profession==dev){
+      arr.splice(i,1)
+    }  
+  }
+  console.log(arr)
 }
 
 function concatenateArray() {
   //Write your code here
+  console.log(arr.concat(ans))
+  
 }
